a38ed4e674bc7d08e22c36106544184bf17b7523
SakethPavanGoti <sakethpavan21@gmail.com>
2024-05-23 01:24:33 +0530

    add more changes

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,15 +1,25 @@
 package com.example;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.*;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.transforms.*;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.util.ShardedKey;
+import org.jetbrains.annotations.NotNull;
+import org.joda.time.Duration;
 
 
 /**
@@ -19,31 +29,7 @@ import java.util.Map;
 public class Main
 {
     private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    public static void convertCsvToJson(String[] args){
-        PipelineOptionsFactory.register(MyPipelineOptions.class);
-        MyPipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(MyPipelineOptions.class);
-        Pipeline pipeline = Pipeline.create(options);
-
-        pipeline.apply("ReadCSV", TextIO.read().from(options.getInputFile()))
-                .apply("ParseAndConvertToJSON", ParDo.of(new DoFn<String, String>() {
-                    @ProcessElement
-                    public void processElement(@Element String line, OutputReceiver<String> out) {
-                        try {
-                            // Assuming CSV format: id,name,price
-                            String[] fields = line.split(",");
-                            Product product = new Product(fields[0], fields[1], Integer.parseInt(fields[2]));
-                            String json = objectMapper.writeValueAsString(product);
-                            out.output(json);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }))
-                .apply("WriteJSON", TextIO.write().to(options.getOutputFile()).withSuffix(".json").withoutSharding());
-
-        pipeline.run().waitUntilFinish();
-    }
+    private static final OkHttpClient client = new OkHttpClient();
 
     public static void parseProductsJson(String[] args) {
         PipelineOptionsFactory.register(ProductDataPipeline.class);
@@ -51,35 +37,23 @@ public class Main
         Pipeline pipeline = Pipeline.create(options);
         ProductDTOCoder.registerCoder(pipeline.getCoderRegistry());
 
-        PCollection<String> input = pipeline.apply("ReadJSON", TextIO.read().from(options.getInputFile()));
-        PCollection<ProductDTO> transformedProducts = input.apply("ParseProducts", ParDo.of(new DoFn<String, ProductDTO>() {
-            @ProcessElement
-            public void processElement(@Element String json, OutputReceiver<ProductDTO> out) {
-                try {
-                    // Deserialize JSON to ProductDTO object
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    ProductDTO productDTO = objectMapper.readValue(json, ProductDTO.class);
+        PCollection<ProductDTO> transformedProducts = pipeline.apply(FileIO.match().filepattern(options.getInputFile()))
+                .apply(FileIO.readMatches())
+                .apply("ReadEntireFile", ParDo.of(new ReadEntireFileFn()))
+                .apply("ParseJsonToProductDTO", ParDo.of(new ParseJsonToProductDTOFn()));
 
-                    // Update description field
-                    updateDescription(productDTO);
+        PCollection<KV<String, ProductDTO>> kvProducts = transformedProducts.apply("AssignKey", ParDo.of(new AssignKeyFn()));
 
-                    // Output ProductDTO
-                    out.output(productDTO);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }));
-        transformedProducts.apply("CallAPI", ParDo.of(new DoFn<ProductDTO, Void>() {
-            @ProcessElement
-            public void processElement(@Element ProductDTO productDTO) {
-                // Call API with ProductDTO
-                // Implement API call logic here
-                // Example:
-                // apiClient.createProduct(productDTO);
-                System.out.println("product DTO {}");
-            }
-        }));
+        PCollection<KV<ShardedKey<String>, Iterable<ProductDTO>>> batchedProducts = kvProducts
+                .apply("BatchProducts", GroupIntoBatches.<String, ProductDTO>ofSize(10)
+                        .withMaxBufferingDuration(Duration.standardSeconds(30))
+                        .withShardedKey());
+
+        PCollection<KV<String, Iterable<ProductDTO>>> unshardedBatches = batchedProducts.apply("UnshardBatches", ParDo.of(new UnshardBatchesFn()));
+
+        unshardedBatches.apply("CallAPI", ParDo.of(new CallApiFn()));
+
+        pipeline.run().waitUntilFinish();
     }
 
     private static void updateDescription(ProductDTO productDTO) {
@@ -97,9 +71,89 @@ public class Main
         }
     }
 
+    static class ReadEntireFileFn extends DoFn<FileIO.ReadableFile, String> {
+        @ProcessElement
+        public void processElement(ProcessContext c) throws Exception {
+            FileIO.ReadableFile file = c.element();
+            String content = new String(Files.readAllBytes(Paths.get(file.getMetadata().resourceId().toString())), StandardCharsets.UTF_8);
+            c.output(content);
+        }
+    }
+
+    static class ParseJsonToProductDTOFn extends DoFn<String, ProductDTO> {
+        @ProcessElement
+        public void processElement(ProcessContext c) throws Exception {
+            String jsonString = c.element();
+            JsonNode jsonArray = objectMapper.readTree(jsonString);
+
+            if (jsonArray.isArray()) {
+                for (JsonNode jsonObject : jsonArray) {
+                    ProductDTO productDTO = objectMapper.treeToValue(jsonObject, ProductDTO.class);
+                    c.output(productDTO);
+                }
+            } else {
+                // Handle case where the input is not an array (optional)
+                ProductDTO productDTO = objectMapper.treeToValue(jsonArray, ProductDTO.class);
+                c.output(productDTO);
+            }
+        }
+    }
+
+    static class AssignKeyFn extends DoFn<ProductDTO, KV<String, ProductDTO>> {
+        @ProcessElement
+        public void processElement(@Element ProductDTO productDTO, OutputReceiver<KV<String, ProductDTO>> out) {
+            out.output(KV.of("key", productDTO));
+        }
+    }
+
+    static class UnshardBatchesFn extends DoFn<KV<ShardedKey<String>, Iterable<ProductDTO>>, KV<String, Iterable<ProductDTO>>> {
+        @ProcessElement
+        public void processElement(@Element KV<ShardedKey<String>, Iterable<ProductDTO>> kv, OutputReceiver<KV<String, Iterable<ProductDTO>>> out) {
+            out.output(KV.of(kv.getKey().getKey(), kv.getValue()));
+        }
+    }
+
+    static class CallApiFn extends DoFn<KV<String, Iterable<ProductDTO>>, Void> {
+        @ProcessElement
+        public void processElement(@Element KV<String, Iterable<ProductDTO>> kv) throws Exception {
+            List<ProductDTO> products = new ArrayList<>();
+            kv.getValue().forEach(products::add);
+
+            // Convert the list of ProductDTO to JSON
+            String json = objectMapper.writeValueAsString(products);
+
+            // Create the request body
+            RequestBody body = RequestBody.create(json, MediaType.get("application/json; charset=utf-8"));
+
+            // Create the HTTP POST request
+            Request request = new Request.Builder()
+                    .url("http://localhost:8080/products")
+                    .post(body)
+                    .build();
+
+            // Send the request and handle the response
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        System.out.println("Response status code: " + response.code());
+                        System.out.println("Response body: " + response.body());
+                    } else {
+                        System.err.println("Request failed with status code: " + response.code());
+                        System.err.println("Response body: " + response.body());
+                    }
+                }
+            });
+        }
+    }
+
     public static void main( String[] args ) {
-        convertCsvToJson(args);
-//        parseProductsJson(args);
+        parseProductsJson(args);
     }
 
 }

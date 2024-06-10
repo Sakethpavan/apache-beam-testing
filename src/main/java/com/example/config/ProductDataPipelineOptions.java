abf26d22c7e591b458ed8e9936415de03c2798ac
SakethPavanGoti <sakethpavan21@gmail.com>
2024-06-10 12:44:14 +0530

    updated pom and pipeline options

--- src/main/java/com/example/config/ProductDataPipelineOptions.java
+++ src/main/java/com/example/config/ProductDataPipelineOptions.java
@@ -1,9 +1,10 @@
 package com.example.config;
 
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 
-public interface ProductDataPipelineOptions extends org.apache.beam.sdk.options.PipelineOptions {
+public interface ProductDataPipelineOptions extends DataflowPipelineOptions {
 
     @Description("Path of the file to read from")
     @Default.String("input.json")
@@ -11,27 +12,22 @@ public interface ProductDataPipelineOptions extends org.apache.beam.sdk.options.
     void setInputFile(String value);
 
     @Description("Akeneo pim host")
-//    @Default.String("https://pim-8f53d76134.trial.akeneo.cloud")
     String getHostUrl();
     void setHostUrl(String value);
 
     @Description("Akeneo client Id")
-//    @Default.String("8_66ezeyfxjbgocsk0w4cgokw0k8wooogw44kogwccos4gswo40w")
     String getClientId();
     void setClientId(String value);
 
     @Description("Akeneo secret")
-//    @Default.String("49x98j1wqtmosg0okcw48os0kg00o0swoco48o4kos84s8w8w0")
     String getSecret();
     void setSecret(String value);
 
     @Description("username")
-//    @Default.String("postman_1986")
     String getUsername();
     void setUsername(String value);
 
     @Description("password")
-//    @Default.String("755677568")
     String getPassword();
     void setPassword(String value);
 }
